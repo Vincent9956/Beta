e679b7158e1871c977891db9baf1839a6c1d7ee4
Vincent <153116267+Vincent9956@users.noreply.github.com>
2025-10-25 23:19:54 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+
+
 const express = require('express');
           const app = express();
           const PORT = 3000;
+
+          const API_KEY = 'sk_live_1234567890abcdef';
+
 
           app.use(express.json());
           app.use(express.urlencoded({ extended: true }));
